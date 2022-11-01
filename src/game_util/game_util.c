950e1feb45744441343bede50ae76be8065dff1f
Rahmah Putri Azzahra <93776934+rahmaptr@users.noreply.github.com>
2022-11-01 11:48:25 +0700

    Update game_util.c

--- src/game_util/game_util.c
+++ src/game_util/game_util.c
@@ -1,5 +1,4 @@
 // create game, list game, queue game, delete game, play game, skip game
 #include <stdio.h>
 #include <stdlib.h>
-#include "../ADT/queue/queue.h"
-#include "../ADT/array/arraydin.h"
+#include "game_util.h"
