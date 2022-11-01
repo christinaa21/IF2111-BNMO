98fdd3233407f149b40aa26738cdb1db445a1927
Vincent Franstyo <sorakiriko28814@gmail.com>
2022-11-01 17:20:48 +0700

    changed game_util

--- src/game_util/game_util.c
+++ src/game_util/game_util.c
@@ -1,10 +1,9 @@
 // create game, list game, queue game, delete game, play game, skip game
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
+
 #include "./game_util.h"
 #include "../ADT/queue/queue.h"
 #include "../ADT/array/arraydin.h"
-=======
+
 #include "game_util.h"
->>>>>>> 950e1feb45744441343bede50ae76be8065dff1f
