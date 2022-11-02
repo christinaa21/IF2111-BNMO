58487bad6c638205f8cd006d6ef2cdf36dabfc20
karinarahadiani <18221104@std.stei.itb.ac.id>
2022-11-02 15:23:54 +0700

    Update random_number_generator.h

---
+++ src/games/random_number_generator/random_number_generator.h
@@ -0,0 +1,20 @@
+#ifndef __RANDOM_NUMBER_GENERATOR__
+#define __RANDOM_NUMBER_GENERATOR__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../../../boolean.h"
+#include "../../main_util/main_util.h"
+#include "../../ADT/array/arraydin.h"
+#include "../../ADT/mesinkata/mesinkata.h"
+#include "../../ADT/prioQueue/prioqueue.h"
+
+
+void RNG();
+// -	Permainan dimulai dengan menyediakan sebuah number random dari sistem
+// -    User akan memasukan sebuah angka untuk menebak
+// -    Sistem akan memberi informasi apakah nomor tebakan lebih besar atau lebih kecil
+// -    User akan terus memasukan input nomor hingga berhasil tertebak
+
+#endif
