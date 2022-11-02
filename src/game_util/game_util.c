60b742442559a3acf8d7d876d087f9f5f39665c7
christinaa21 <16521224@mahasiswa.itb.ac.id>
2022-11-02 17:57:46 +0700

    Update playGame

--- src/game_util/game_util.c
+++ src/game_util/game_util.c
@@ -38,40 +38,32 @@ void displayQueueGame();
 // I.S. Program telah berjalan
 // F.S. Menampilkan antrian game pengguna.
 
-void playGame(Queue qGame) {
+void playGame(Queue *qGame) {
     int game;
     displayQueueGame();
-    printf("ENTER GAME: ");
-    scanf("%d", &game);
-    printf("\n");
-    if (isEmpty(qGame)) {
+    if (isEmpty(*qGame)) {
         printf("Maaf, antrian game-mu kosong. Silakan menambahkan game ke antrian terlebih dahulu dengan mengetik command: QUEUE GAME\n");
     } else {
-        if (game > length(qGame)) {
-            printf("Maaf, nomor game yang Anda masukkan lebih dari jumlah antrian game yang Anda miliki.\nSilakan masukkan nomor game yang sesuai.\n");
-        } else if (game < 1) {
-            printf("Maaf, nomor game yang Anda masukkan tidak valid. Nomor game harus lebih dari 0.");
+        /* Asumsi: Game yg ada di qGame merupakan nomor-nomor game yang ada di List Game.
+                    Game nomor 1-5 mengikuti game yang ada di file konfigurasi, yaitu:
+                    1 = RNG
+                    2 = Diner Dash
+                    3 = Dinosaur in earth
+                    4 = Risewoman
+                    5 = Eiffel Tower */
+        dequeue(qGame, &game);
+        if (game == 1) {
+            RNG();
+        } else if (game == 2) {
+            DinerDash();
+        } else if (game >= 3) {
+            printf("Game DINOSAUR IN EARTH masih dalam maintenance, belum dapat dimainkan. Silakan pilih game lain.\n");
+        } else if (game >= 4) {
+            printf("Game RISEWOMAN masih dalam maintenance, belum dapat dimainkan. Silakan pilih game lain.\n");
+        } else if (game >= 5) {
+            printf("Game EIFFEL TOWER masih dalam maintenance, belum dapat dimainkan. Silakan pilih game lain.\n");
         } else {
-            /* Asumsi: Game yg ada di qGame merupakan nomor-nomor game yang ada di List Game.
-                       Game nomor 1-5 mengikuti game yang ada di file konfigurasi, yaitu:
-                       1 = RNG
-                       2 = Diner Dash
-                       3 = Dinosaur in earth
-                       4 = Risewoman
-                       5 = Eiffel Tower */
-            if (qGame.buffer[game-1] == 1) {
-                RNG();
-            } else if (qGame.buffer[game-1] == 2) {
-                DinerDash();
-            } else if (qGame.buffer[game-1] >= 3) {
-                printf("Game DINOSAUR IN EARTH masih dalam maintenance, belum dapat dimainkan. Silakan pilih game lain.\n");
-            } else if (qGame.buffer[game-1] >= 4) {
-                printf("Game RISEWOMAN masih dalam maintenance, belum dapat dimainkan. Silakan pilih game lain.\n");
-            } else if (qGame.buffer[game-1] >= 5) {
-                printf("Game EIFFEL TOWER masih dalam maintenance, belum dapat dimainkan. Silakan pilih game lain.\n");
-            } else {
-                gameTambahan();
-            }
+            gameTambahan();
         }
     }
 }
