eac5d41541e961514f9ee724cb30dbf0056b40d7
Vincent Franstyo <sorakiriko28814@gmail.com>
2022-11-10 12:32:42 +0700

    edited readme

--- README.md
+++ README.md
@@ -2,20 +2,23 @@
 
 ### IF2111 ALGORITMA DAN STRUKTUR DATA
 
-Deskripsi BNMO
+BNMO adalah sebuah sistem game yang diakses melalui command-line interface. Program ini memiliki empat fungsi utama yaitu memainkan game, menambahkan game, menghapus game, dan mengurutkan game yang akan dimainkan sesuai antrian yang dimasukan. Jadi user bisa menyunting game mereka juga memainkannya dalam program BNMO ini. 
+<br>
+Pada pemenuhan tugas besar kali ini, pembuatan program BNMO juga diikuti oleh pemenuhan pembuatan dua game utama yaitu Diner Dash dan Random Number Generator. Diner Dash adalah game yang diadaptasi dari sistem pelayanan pada restoran, dimana user yang memainkan game ini harus bisa mengatur sistem antrian pelayanan kepada pelanggan restorannya. Sedangkan dalam game Random Number Generator user diminta menebak sebuah angka acak yang dibuat oleh mesin dengan clue tebakan apakah lebih besar atau lebih kecil. Selain game-game di atas, ada dua game bonus yang berhasil ditambahkan yaitu Tic Tac Toe yang diadaptasi dari permainan Tic Tac Toe pada umumnya dan Hangman yang di dalamnya terdapat kata-kata yang harus ditebak dan masih bernuansa topik sehari-hari dari mahasiswa STI. 
+<br>
 
 Game ini menggunakan aplikasi abstract data type (ADT) dalam bahasa C
 
 ## Struktur Folder
 Berikut adalah struktur folder dari project BNMO kami:
 - Folder `bin` yang berisi ***...***
-- Folder `Data` yang berisi data data pengguna
+- Folder `Data` yang berisi data-data pengguna
 - Folder `Docs` yang berisi dokumen-dokumen yang kami gunakan dan perlukan
 - Folder `src` yang berisi :
     - folder `ADT` yang berguna untuk menyimpan seluruh ADT yang kami gunakan
     - folder `game_util` yang digunakan untuk menyimpan seluruh utilities dalam game yang kami perlukan (create game, list game, queue game, delete game, play game, skip game)
     - folder `main_util` yang digunakan untuk menyimpan utilities yang diperlukan untuk menyimpan utilities utama (start, load, save, quit, help)
-    - folder `games` yang digunakan untuk menyimpan seluruh game kami (RNG dan Diner Dash)
+    - folder `games` yang digunakan untuk menyimpan seluruh game kami (RNG, Diner Dash, Hangman, TicTacToe)
 - file `boolean.h` untuk digunakan saat memerlukan header boolean
 - file `config.txt` sebagai file awal yang akan dijalankan saat fungsi `start` dijalankan
 - program utama kami yang disimpan dalam `main.c`
@@ -59,7 +62,7 @@ make compile
 5. Program telah berjalan dan silahkan nikmati perjalanan anda
 
 ## Kontributor
-Program ini dikembangkan sebagai salah satu tugas besar mata kuliah Algoritma dan Struktur Data. Program ini dikembangkan oleh (K02 Kelompok Al-Stroke-Dead *(atau isi nama lain)*)
+Program ini dikembangkan sebagai salah satu tugas besar mata kuliah Algoritma dan Struktur Data. Program ini dikembangkan oleh (K02 Kelompok **Al-Stroke-Dead** *(atau isi nama lain)*)
 1. Rahmah Putri Azzahra `18219052`<br/>
     kontrib
 2. Vincent Franstyo `18221100`<br/>
--- src/games/hangman/hangman.c
+++ src/games/hangman/hangman.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include "hangman.h"
 
-void capsLock(char* huruf) {
-    if ((huruf[0] >= 97) && (huruf[0] <= 122)) {
-        huruf[0]-=32;
+void capsLock(char *huruf)
+{
+    if ((huruf[0] >= 97) && (huruf[0] <= 122))
+    {
+        huruf[0] -= 32;
     }
 }
 /* Membuat semua huruf menjadi uppercase */
 
-void hangMan() {
+void hangMan()
+{
     // Game bonus
     ArrayDin ArrayKata;
     ArrayKata = MakeArrayDin();
@@ -40,7 +43,7 @@ void hangMan() {
 
     srand(time(NULL));
     int X = rand() % 25;
-    char* tebakan = Get(ArrayKata, X);
+    char *tebakan = Get(ArrayKata, X);
     Word tertebak = StringtoWord(tebakan);
     Word tebakan1 = StringtoWord(tebakan);
 
@@ -60,7 +63,8 @@ void hangMan() {
     ArrayBlank = MakeArrayDin();
     int i = 0;
     int ctr = 0;
-    while (tebakan[i] != '\0') {
+    while (tebakan[i] != '\0')
+    {
         InsertLast(&ArrayBlank, "_ ");
         i++;
     }
@@ -74,36 +78,48 @@ void hangMan() {
     printf("Kata yang harus ditebak:\n");
     PrintArrayDin(ArrayBlank);
     printf("%s\n\n", Get(ArrayHangman, salah));
-    while ((salah < 8) && (benar < ctr)) {
+    while ((salah < 8) && (benar < ctr))
+    {
         STARTINPUTKATA();
-        if (currentWord.Length != 1) {
+        if (currentWord.Length != 1)
+        {
             printf("Maaf, Anda hanya diperbolehkan untuk memasukkan 1 huruf saja.\n");
-        } else {
-            char* huruf = WordToString(currentWord);
+        }
+        else
+        {
+            char *huruf = WordToString(currentWord);
             capsLock(huruf);
-            if (IsInWord(huruf, tertebak)) {
-                for (int i = 0;i<ctr;i++) {
-                    if (huruf[0] == tebakan[i]) {
+            if (IsInWord(huruf, tertebak))
+            {
+                for (int i = 0; i < ctr; i++)
+                {
+                    if (huruf[0] == tebakan[i])
+                    {
                         Set(&ArrayBlank, i, huruf);
                         tertebak.TabWord[i] = '$';
                         benar++;
                     }
                 }
                 printf("\nYey! Kamu benar! :D\n");
-            } else if ((!IsInWord(huruf, tertebak)) && IsInWord(huruf, tebakan1)) {
+            }
+            else if ((!IsInWord(huruf, tertebak)) && IsInWord(huruf, tebakan1))
+            {
                 printf("\nHuruf sudah ditebak.\n");
-            } else if (!IsInWord(huruf, tertebak)) {
+            }
+            else if (!IsInWord(huruf, tertebak))
+            {
                 salah++;
                 printf("\nYahh, kamu salah :(\n");
-                printf("Sisa kesempatan menebak: %d\n", (8-salah));
+                printf("Sisa kesempatan menebak: %d\n", (8 - salah));
             }
         }
         printf("\nKata yang harus ditebak:\n");
         PrintArrayDin(ArrayBlank);
         printf("%s\n\n", Get(ArrayHangman, salah));
     }
-    score=score-(salah*salah);
-    if (salah == 8) {
+    score = score - (salah * salah);
+    if (salah == 8)
+    {
         score = 0;
         printf("Kata yang benar adalah %s\n", tebakan);
     }
