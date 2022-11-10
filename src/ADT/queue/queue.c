94c0b8e756c4e51bb09a6eb157800355e2fa6659
karinarahadiani <18221104@std.stei.itb.ac.id>
2022-11-10 11:41:55 +0700

    Update queue.c

--- src/ADT/queue/queue.c
+++ src/ADT/queue/queue.c
@@ -2,35 +2,44 @@
 #include "queue.h"
 #include "boolean.h"
 
-/* *** Kreator *** */
+/* *** KREATOR *** */
+/* I.S : sembarang */
+/* F.S : Sebuah q kosong terbentuk dengan kondisi sbb: */
+/* 		- Index head bernilai IDX_UNDEF */
+/* 		- Index tail bernilai IDX_UNDEF */
+/* Proses : Melakukan alokasi, membuat sebuah q kosong */
 void CreateQueue(Queue *q)
 {
-	/* I.S. sembarang */
-	/* F.S. Sebuah q kosong terbentuk dengan kondisi sbb: */
-	/* - Index head bernilai IDX_UNDEF */
-	/* - Index tail bernilai IDX_UNDEF */
-	/* Proses : Melakukan alokasi, membuat sebuah q kosong */
 	IDX_HEAD(*q) = IDX_UNDEF;
 	IDX_TAIL(*q) = IDX_UNDEF;
 }
 
-/* ********* Prototype ********* */
+/* ********* PROTOTYPE ********* */
+/* Fungsi yang mengirim true jika q kosong: lihat definisi di atas */
 boolean isEmpty(Queue q)
 {
-	/* Mengirim true jika q kosong: lihat definisi di atas */
+	/*KAMUS LOKAL*/
+
+	/*ALGORITMA*/
 	return ((IDX_HEAD(q) == IDX_UNDEF) && (IDX_TAIL(q) == IDX_UNDEF));
 }
 
+/* Fungsi yang mengirim true jika tabel penampung elemen q sudah penuh */
+/* yaitu IDX_TAIL akan selalu di belakang IDX_HEAD dalam buffer melingkar*/
 boolean isFull(Queue q)
 {
-	/* Mengirim true jika tabel penampung elemen q sudah penuh */
-	/* yaitu IDX_TAIL akan selalu di belakang IDX_HEAD dalam buffer melingkar*/
+	/*KAMUS LOKAL*/
+
+	/*ALGORITMA*/
 	return (length(q) == CAPACITY);
 }
 
+/* Fungsi yang mengirimkan banyaknya elemen queue. Mengirimkan 0 jika q kosong. */
 int length(Queue q)
 {
-	/* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika q kosong. */
+	/*KAMUS LOKAL*/
+
+	/*ALGORITMA*/
 	if (isEmpty(q))
 	{
 		return 0;
@@ -48,13 +57,16 @@ int length(Queue q)
 	}
 }
 
-/* *** Primitif Add/Delete *** */
+/* *** OPERASI OPERASI *** */
+/* Proses: Menambahkan val pada q dengan aturan FIFO */
+/* I.S : q mungkin kosong, tabel penampung elemen q TIDAK penuh */
+/* F.S : val menjadi TAIL yang baru, IDX_TAIL "mundur" dalam buffer melingkar. */
 void enqueue(Queue *q, ElTypeQueue val)
 {
-	/* Proses: Menambahkan val pada q dengan aturan FIFO */
-	/* I.S. q mungkin kosong, tabel penampung elemen q TIDAK penuh */
-	/* F.S. val menjadi TAIL yang baru, IDX_TAIL "mundur" dalam buffer melingkar. */
-	// empty case
+	/*KAMUS LOKAL*/
+
+	/*ALGORITMA*/
+	//kasus kosong 
 	if (isEmpty(*q))
 	{
 		IDX_HEAD(*q) = 0;
@@ -74,20 +86,21 @@ void enqueue(Queue *q, ElTypeQueue val)
 	}
 }
 
+/* Proses: Menghapus val pada q dengan aturan FIFO */
+/* I.S : q tidak mungkin kosong */
+/* F.S : val = nilai elemen HEAD pd I.S., IDX_HEAD "mundur";
+		 q mungkin kosong */
 void dequeue(Queue *q, ElTypeQueue *val)
 {
-	/* Proses: Menghapus val pada q dengan aturan FIFO */
-	/* I.S. q tidak mungkin kosong */
-	/* F.S. val = nilai elemen HEAD pd I.S., IDX_HEAD "mundur";
-			q mungkin kosong */
+	/*KAMUS LOKAL*/
 	(*val).Length = HEAD(*q).Length;
 	int i;
+	/*ALGORITMA*/
 	for (i = 0; i < (*val).Length; i++)
 	{
 		(*val).TabWord[i] = HEAD(*q).TabWord[i];
 	}
 
-	//
 	if (IDX_HEAD(*q) == IDX_TAIL(*q))
 	{
 		IDX_HEAD(*q) = IDX_UNDEF;
@@ -101,10 +114,15 @@ void dequeue(Queue *q, ElTypeQueue *val)
 }
 
 /* *** Display Queue *** */
+/* Prosedur untuk menampilkan Queue secara transversal*/
+/* I.S : Queue terdefinisi */
+/* F.S : Queue terlihat dilayar */
 void displayQueue(Queue q)
 {
+	/*KAMUS LOKAL*/
 	int i, temp1, temp2;
 	ElTypeQueue val;
+	/*ALGORITMA*/
 	if (isEmpty(q))
 	{
 		printf("[]");
@@ -132,16 +150,18 @@ void displayQueue(Queue q)
 	printf("\n");
 }
 
+/* Proses: Menyalin isi dari queueInput ke queueOutput */
+/* I.S : queueInput mungkin kosong, tabel penampung elemen queueInput TIDAK penuh */
+/* F.S : queueOutput memiliki isi queue yang sama */
 void copyQueue(Queue *queueInput, Queue *queueOutput)
 {
-	/* Proses: Menyalin isi dari queueInput ke queueOutput */
-	/* I.S. queueInput mungkin kosong, tabel penampung elemen queueInput TIDAK penuh */
-	/* F.S. queueOutput memiliki isi queue yang sama */
+	/*KAMUS LOKAL*/
 	int i, len;
 	Queue q;
 	ElTypeQueue val;
 	CreateQueue(&q);
 	len = length(*queueInput);
+	/*ALGORITMA*/
 	for (i = 0; i < len; i++)
 	{
 		dequeue(queueInput, &val);
@@ -156,12 +176,14 @@ void copyQueue(Queue *queueInput, Queue *queueOutput)
 	}
 }
 
+/* Fungsi yang mengembalikan true jika x merupakan elemen dari q */
 boolean isInQueue(Queue q, ElTypeQueue x)
 {
-	/* Mengembalikan true jika x merupakan elemen dari q */
+	/*KAMUS LOKAL*/
 	ElTypeQueue val;
 	char *xchar = WordToString(x);
 	boolean notfound = true;
+	/*ALGORITMA*/
 	while ((IDX_HEAD(q) != IDX_UNDEF) && notfound)
 	{
 		dequeue(&q, &val);
