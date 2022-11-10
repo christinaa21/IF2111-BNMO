45425328fa1e65e68a50fc2c7078632fc6343c7d
christinaa21 <16521224@mahasiswa.itb.ac.id>
2022-11-11 03:29:21 +0700

    Update driver of queue

--- src/ADT/queue/driver_queue.c
+++ src/ADT/queue/driver_queue.c
@@ -4,48 +4,67 @@
 int main() {
     Word val;
     Queue q1, q2;
-    char *pita = "../../../Data/gamelist.txt";
     ElTypeQueue g1, g2, g3, g4, g5;
-    CreateQueue(&q1);
+    printf("Test ADT Queue\n");
+    printf("----------------------\n\n");
 
-    enqueue(&q1, g1);
-    enqueue(&q1, g2);
-    enqueue(&q1, g3);
-    enqueue(&q1, g4);
-    enqueue(&q1, g5);
+    printf("Membuat array kosong\n");
+    CreateQueue(&q1);
     printf("q1 = ");
     displayQueue(q1);
+    printf("isEmpty: %d\n", isEmpty(q1));
+    printf("isFull: %d\n", isFull(q1));
+    printf("length: %d\n", length(q1));
+    printf("----------------------\n");
 
+    printf("Memasukkan (Enqueue) elemen ke dalam queue\n");
+    enqueue(&q1, StringtoWord("Tes1"));
+    enqueue(&q1, StringtoWord("Tes2"));
+    enqueue(&q1, StringtoWord("Tes3"));
+    enqueue(&q1, StringtoWord("Tes4"));
+    enqueue(&q1, StringtoWord("Tes5"));
+    printf("q1 = ");
+    displayQueue(q1);
+    printf("isEmpty: %d\n", isEmpty(q1));
+    printf("isFull: %d\n", isFull(q1));
+    printf("length: %d\n", length(q1));
+    printf("----------------------\n");
+
+    printf("Tes isInQueue\n");
+    printf("isInQueue(Tes2) = %d\n", isInQueue(q1, StringtoWord("Tes2")));
+    printf("isInQueue(Tes6) = %d\n", isInQueue(q1, StringtoWord("Tes6")));
+    printf("----------------------\n");
+
+    printf("Menyalin q1 ke q2 dengan copyQueue\n");
+    CreateQueue(&q2);
     copyQueue(&q1, &q2);
+    printf("q1 = ");
+    displayQueue(q1);
     printf("q2 = ");
     displayQueue(q2);
+    printf("----------------------\n");
 
-    printf("\n===========================\n");
-
-    //dequeue(&q1, &val);
-    //dequeue(&q1, &val);
-    //enqueue(&q1, 11);
-
-    while (length(q1) > 0) {
+    printf("Menghapus (Dequeue) elemen queue\n");
+    while (!isEmpty(q1)) {
         printf("q1 = ");
         displayQueue(q1);
-        printf("panjang : %d\n",length(q1));
-        printf("full : %d\n",isFull(q1));
-        printf("empty : %d\n",isEmpty(q1));
-        printf("head: %d\n",IDX_HEAD(q1));
-        printf("tail: %d\n",IDX_TAIL(q1));
-        printf("===========================\n");
+        printf("isEmpty = %d\n", isEmpty(q1));
+        printf("isFull = %d\n", isFull(q1));
+        printf("length = %d\n", length(q1));
+        printf("head = %d\n",IDX_HEAD(q1));
+        printf("tail = %d\n",IDX_TAIL(q1));
+        printf("----------------------\n");
         dequeue(&q1, &val); 
-        printf("dequeued : %d\n",val);
+        printf("dequeued = %s\n",WordToString(val));
     }
-
+    printf("Keadaan saat queue kosong\n");
     displayQueue(q1);
-
-    printf("panjang: %d\n",length(q1));
-    printf("full : %d\n",isFull(q1));
-    printf("empty : %d\n",isEmpty(q1));
-    printf("head: %d\n",IDX_HEAD(q1));
-    printf("tail: %d\n",IDX_TAIL(q1));
+    printf("isEmpty = %d\n", isEmpty(q1));
+    printf("isFull = %d\n", isFull(q1));
+    printf("length = %d\n", length(q1));
+    printf("head = %d\n",IDX_HEAD(q1));
+    printf("tail = %d\n",IDX_TAIL(q1));
+    printf("----------------------\n");
 
     return 0;
 }
