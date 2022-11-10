0bd3828183d6b00a5a81b9e810f1ff9194aaad6b
karinarahadiani <18221104@std.stei.itb.ac.id>
2022-11-10 11:06:43 +0700

    Update mesinkarakter.h

--- src/ADT/mesinkata/mesinkarakter.h
+++ src/ADT/mesinkata/mesinkarakter.h
@@ -11,35 +11,41 @@
 extern char currentChar;
 extern boolean EOP;
 
-void START(char * FILE);
-/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
+/* ********** KONSTRUKTOR ********** */
+/* Prosedur yang memproses dari mesin siap dioperasikan. Pita disiapkan untuk dibaca.
    Karakter pertama yang ada pada pita posisinya adalah pada jendela.
    Pita baca diambil dari FILE.
-   I.S. : sembarang
-   F.S. : currentChar adalah karakter pertama pada pita
+   I.S : sembarang
+   F.S : currentChar adalah karakter pertama pada pita
           Jika currentChar != MARK maka EOP akan padam (false)
           Jika currentChar = MARK maka EOP akan menyala (true) */
 
+void START(char * FILE);
+
+/* Prosedur yang memproses dari mesin siap dioperasikan. Pita disiapkan untuk dibaca.
+ * Karakter pertama yang ada pada pita posisinya adalah pada jendela.
+ * Pita baca diambil dari stdin.
+ * I.S : sembarang
+ * F.S : currentChar adalah karakter pertama pada pita
+ *        Jika currentChar != MARK maka EOP akan padam (false)
+ *        Jika currentChar = MARK maka EOP akan menyala (true) */
 void STARTINPUT();
-/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
-   Karakter pertama yang ada pada pita posisinya adalah pada jendela.
-   Pita baca diambil dari stdin.
-   I.S. : sembarang
-   F.S. : currentChar adalah karakter pertama pada pita
-          Jika currentChar != MARK maka EOP akan padam (false)
-          Jika currentChar = MARK maka EOP akan menyala (true) */
 
+/* Prosedur untuk memajukan pita satu karakter.
+ * I.S : Karakter pada jendela = currentChar, currentChar != MARK
+ * F.S : currentChar adalah karakter berikutnya dari currentChar yang lama,
+ *        currentChar mungkin = MARK
+ *        Jika  currentChar = MARK maka EOP akan menyala (true) */
 void ADV();
-/* Pita dimajukan satu karakter.
-   I.S. : Karakter pada jendela = currentChar, currentChar != MARK
-   F.S. : currentChar adalah karakter berikutnya dari currentChar yang lama,
-          currentChar mungkin = MARK
-          Jika  currentChar = MARK maka EOP akan menyala (true) */
 
+/* ********** SELEKTOR ********** */
+/* Fungsi yang mengembalikan nilai currentChar 
+   Prekondisi : program telah berjalan */
 char GetCC();
-/* Mengirimkan currentChar */
 
+/* ********** TEST EOP ********** */
+/* Fungsi yang mengirimkan nilai kebenaran jika currentChar adalah EOP
+ * Prekondisi : program telah berjalan */
 boolean IsEOP();
-/* Mengirimkan true jika currentChar = MARK */
 
 #endif
