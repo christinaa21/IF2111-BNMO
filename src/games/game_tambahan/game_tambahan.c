df4f43d6b33e73731b481b37dcb882dcfdd074b0
karinarahadiani <18221104@std.stei.itb.ac.id>
2022-11-10 12:03:02 +0700

    Update game_tambahan.c

--- src/games/game_tambahan/game_tambahan.c
+++ src/games/game_tambahan/game_tambahan.c
@@ -3,6 +3,9 @@
 #include <time.h>
 #include "game_tambahan.h"
 
+/* Game buatan pemain yang dibuat dengan command CREATE GAME
+ * Ketika dimainkan, akan langsung selesai & game over dengan skor akhir berupa integer random. */
+
 void gameTambahan(){
     printf("Game Over!\n");
     int skor;
@@ -11,5 +14,3 @@ void gameTambahan(){
     printf("Skor yang Anda peroleh: %d\n", skor);
 
 }
-// Game buatan pemain yang dibuat dengan command CREATE GAME
-// Ketika dimainkan, akan langsung selesai & game over dengan skor akhir berupa integer random.
