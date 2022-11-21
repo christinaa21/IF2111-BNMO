19f2575b2f64c9caaf76eb00ad0d5328cdfe85ae
sulsul2 <110590472+sulsul2@users.noreply.github.com>
2022-11-21 14:13:58 +0700

    Update Linkedlist and Add ADT Point

--- src/ADT/LinkedList/listdp.c
+++ src/ADT/LinkedList/listdp.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "listdp.h"
 
 boolean IsEmpty (List L){
@@ -9,12 +10,13 @@ void CreateEmpty (List *L){
     First(*L) = Nil;
     Last(*L) = Nil;
 }
-address Alokasi (infotype X){
+address Alokasi (infotype X, POINT Y){
     address P = (address) malloc (sizeof(ElmtList));
     if (P!=Nil){
         Info(P) = X;
         Next(P) = Nil;
         Prev(P) = Nil;
+        Pos(P) = Y;
     }
     return P;
 }
@@ -23,11 +25,11 @@ void Dealokasi (address P){
     free(P);
 }
 
-address Search (List L, infotype X){
+address Search (List L, POINT Y){
     address P = First(L);
     boolean ada = false; 
     while (P!=Nil && !ada){
-        if(Info(P) == X){
+        if(Pos(P).x == Y.x && Pos(P).y == Y.y){
             ada = true;
         }
         else{
@@ -42,23 +44,8 @@ address Search (List L, infotype X){
     }
 }
 
-void InsVFirst (List *L, infotype X){
-    address P = Alokasi(X);
-    if (P!=Nil){
-        if(!IsEmpty(*L)){
-            Next(P) = First(*L);
-            Prev(First(*L)) = P;
-            First(*L) = P;
-        }
-        else {
-            First(*L) = P;
-            Last (*L) = P;
-        }
-    }
-}
-
-void InsVLast (List *L, infotype X){
-    address P = Alokasi(X);
+void InsVLast (List *L, infotype X, POINT Y){
+    address P = Alokasi(X,Y);
     if (P!=Nil){
         if(!IsEmpty(*L)){
             Next(Last(*L)) = P;
@@ -70,82 +57,6 @@ void InsVLast (List *L, infotype X){
             Last(*L) = P;
         }
     }
-}
-
-void DelVFirst (List *L, infotype *X){
-    address P = First(*L);
-    *X = Info(P);
-    if (Next(P) == Nil){
-        First(*L) = Nil;
-        Last(*L) = Nil;
-    }
-    else {
-        First(*L) = Next(P);
-        Prev(Next(P)) = Nil;
-    }
-    Dealokasi(P);
-}
-
-void DelVLast (List *L, infotype *X){
-    address P = Last(*L);
-    *X = Info(P);
-    if(Prev(P) == Nil){
-        First(*L) = Nil;
-        Last(*L) = Nil;
-    }
-    else {
-        Last(*L) = Prev(P);
-        Next(Prev(P)) = Nil;
-    }
-    Dealokasi(P);
-}
-
-void InsertFirst (List *L, address P){
-    if(IsEmpty(*L)){
-        First(*L) = P;
-        Last(*L) = P;
-    }
-    else {
-        Next(P) = First(*L);
-        Prev(First(*L)) = P;
-        First(*L) = P;
-    }
-}
-
-void InsertLast (List *L, address P){
-    if(IsEmpty(*L)){
-        First(*L) = P;
-        Last(*L) = P;
-    }
-    else {
-        Next(Last(*L)) = P;
-        Prev(P) = Last(*L);
-        Last(*L) = P;   
-    }
-}
-
-void InsertAfter (List *L, address P, address Prec){
-    if (Next(Prec) == Nil){
-        Last(*L) = P;
-    }
-    else {
-        Prev(Next(Prec)) = P;
-    }
-    Next(P) = Next(Prec);
-    Prev(P) = Prec;
-    Next(Prec) = P;
-}
-
-void InsertBefore (List *L, address P, address Succ){
-    if (Prev(Succ) == Nil){
-        First(*L) = P;
-    }
-    else {
-        Next(Prev(Succ)) = P;
-    }
-    Prev(P) = Prev(Succ);
-    Next(P) = Succ;
-    Prev(Succ) = P;
 }
 
 void DelFirst (List *L, address *P){
@@ -172,18 +83,10 @@ void DelLast (List *L, address *P){
     }
 }
 
-void DelP (List *L, infotype X){
-    address P = First(*L);
-    boolean ada = false;
-    while (P!=Nil && !ada){
-        if(Info(P) == X){
-            ada = true;
-        }
-        else {
-            P = Next(P);
-        }
-    }
-    if (ada){
+
+void DelP (List *L, POINT X){
+    address P = Search(*L,X);
+    if (P!=Nil){
         if (Prev(P) == Nil){
             DelFirst(L,&P);
         }
@@ -191,6 +94,15 @@ void DelP (List *L, infotype X){
             DelLast(L,&P);
         }
         else {
+            address Q = Last(*L);
+            address R;
+            while(Q != P){
+                R = Prev(Q);
+                Info(Q) = Info(R);
+                Pos(Q).x = Pos(R).x;
+                Pos(Q).y = Pos(R).y;
+                Q = Prev(Q);
+            }
             Next(Prev(P)) = Next(P);
             Prev(Next(P)) = Prev(P);
         }
@@ -198,31 +110,9 @@ void DelP (List *L, infotype X){
     }
 }
 
-void DelAfter (List *L, address *Pdel, address Prec){
-    *Pdel = Next(Prec);
-    Next(Prec) = Next(*Pdel);
-    if(Next(*Pdel) == Nil){
-        Last(*L) = Prec;
-    }
-    else {
-        Prev(Next(*Pdel)) = Prec;
-    }
-}
-
-void DelBefore (List *L, address *Pdel, address Succ){
-    *Pdel = Prev(Succ);
-    Prev(Succ) = Prev(*Pdel);
-    if (Prev(*Pdel) == Nil){
-        First(*L) = Succ;
-    }
-    else {
-        Next(Prev(*Pdel)) = Succ;
-    }
-}
-
 void PrintForward (List L){
     address P = First(L);
-    if (IsEmpty(L) == Nil){
+    if (IsEmpty(L)){
         printf("[]");
     }
     else {
@@ -242,7 +132,7 @@ void PrintForward (List L){
 
 void PrintBackward (List L){
     address P = Last(L);
-    if (IsEmpty(L) == Nil){
+    if (IsEmpty(L)){
         printf("[]");
     }
     else {
@@ -260,5 +150,8 @@ void PrintBackward (List L){
     }
 }
 
+POINT GetLastPos(List L){
+    return Pos(Last(L));
+}
 
 
