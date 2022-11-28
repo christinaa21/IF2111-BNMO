eca6e01e39d1b987b38cef0f10d483f4c5cf6ba6
karinarahadiani <18221104@std.stei.itb.ac.id>
2022-11-28 22:54:56 +0700

    Update driver_listMap.c

--- src/ADT/map/driver_listMap.c
+++ src/ADT/map/driver_listMap.c
@@ -13,13 +13,19 @@ int main() {
     
     printf("nambahin list map -> print\n");
     insertToList(&L, M);
+    InsertMap(&(L).peta[0], "ponyo", 12);
     printAllList(L);
 
     printf("reset 1 map -> print\n");
-    resetAtMap(&L, 6);
+    resetAtMap(&L, 0);
     printAllList(L);
 
     printf("reset all map -> print\n");
+    printf("before\n"); 
+    InsertMap(&(L).peta[0], "ponyo", 12);
+    InsertMap(&(L).peta[1], "nana", 10);
+    printAllList(L);
+    printf("after\n");
     resetAllMap(&L);
     printAllList(L);
 
